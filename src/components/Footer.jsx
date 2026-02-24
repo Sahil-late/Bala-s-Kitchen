65a743440d40dc43491b30f669b56688fb1f1f13
Sahil-late <latesahil67@gmail.com>
2026-02-24 17:01:06 +0530

    Update contact number in Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="footer fixed bottom-0 w-full z-10 bg-[rgba(0,0,0,0.8)]  text-white py-3 text-center">
       <div className="footer-content">
         <p>© {new Date().getFullYear()} Bala's Kitchen</p>
-        <p>Contact us: +91 9876543210</p>
+        <p>Contact us: +91 8767685939</p>
       </div>
     </footer>
   );
